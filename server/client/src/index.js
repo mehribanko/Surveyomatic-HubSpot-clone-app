deefa32c0d1451657653d127c347edb05cfbc957
DESKTOP-BCRBV2A\marym <mehrikodes@gmail.com>
2023-01-11 10:55:22 +0900

    testFromWork - configuring Header component

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
 import App from './components/App';
+
 
 import { createRoot } from "react-dom/client";
 
