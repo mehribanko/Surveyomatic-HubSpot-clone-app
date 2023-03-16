53c6def20ea8f604e25952409c89025c72ca77b4
Maryko <maryko_h@douzone.com>
2023-03-16 15:34:48 +0900

    testFromWork - testing API endpoint for current user

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
-
-
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
+
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
     render(){
         return (
             <div>
@@ -28,7 +34,7 @@ class App extends Component {
 }
 
 
-export default App;
+export default connect(null, actions) (App);
 
 
 
