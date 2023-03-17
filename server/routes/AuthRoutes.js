f8918452f2252148e234f1dcef38c50e30e2557f
Maryko <maryko_h@douzone.com>
2023-03-17 17:42:09 +0900

    testFromWork - configuring Landing page

--- server/routes/AuthRoutes.js
+++ server/routes/AuthRoutes.js
@@ -6,16 +6,19 @@ module.exports = (app) => {
     passport.authenticate("google", { scope: ["profile", "email"] })  //here the 'google' name refers to GoogleStrategy under the hood (in passport.js file)
   );
 
-  app.get('/auth/google/callback', passport.authenticate("google"));
+  app.get('/auth/google/callback', passport.authenticate("google"),
+  (req,res) => {
+    res.redirect('/surveys')
+  }
+  );
 
   app.get('/api/current_user', (req,res) => {
     res.send(req.user);
   })
   
-
   app.get('/api/logout', (req,res)=>{
     req.logout();
-    res.send(req.user);
+    res.redirect('/');
   })
 };
 
