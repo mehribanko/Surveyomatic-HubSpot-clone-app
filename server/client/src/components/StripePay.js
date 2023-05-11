4d30a7a92d90a36e9329a9d9f3361b423536e776
Maryko <maryko_h@douzone.com>
2023-05-11 16:41:30 +0900

    testFromWork -  configuring StripePay, Header components

---
+++ server/client/src/components/StripePay.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class StripePay extends Component{
+    render(){
+        return(
+
+            <StripeCheckout 
+                amount={500}
+                token={token=> console.log(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            />
+        )
+    }
+}
+
+
+
+export default StripePay;
+
+
