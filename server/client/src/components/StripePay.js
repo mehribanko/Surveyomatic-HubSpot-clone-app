4978c29ddbfcb65cf9e22f989c4ba4de0d2ae625
Maryko <maryko_h@douzone.com>
2023-05-12 14:09:13 +0900

    testFromWork - configuring Stripe Pay

--- server/client/src/components/StripePay.js
+++ server/client/src/components/StripePay.js
@@ -11,7 +11,12 @@ class StripePay extends Component{
                 amount={1000}
                 token={token=> console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+
+            >
+            <button className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-bold rounded-md text-green-800 bg-amber-500 hover:bg-amber-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Add Credits</button> 
+
+            </StripeCheckout>
+
         )
     }
 }
