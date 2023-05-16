fe34c0d720b843296145756dd8cbce186153f8b0
Maryko <maryko_h@douzone.com>
2023-05-16 11:06:26 +0900

    testFromWork - configuring payment charges

--- server/routes/PayRoutes.js
+++ server/routes/PayRoutes.js
@@ -1,10 +1,18 @@
 const keys = require('../config/keys');
-const stripe = require('stripe');
+const stripe = require('stripe')(keys.stripeSecretKey);
 
 
 module.exports = (app) => {
     app.post(
         '/api/stripe', (req,res) => {
+
+            stripe.charges.create({
+                amount: 1000,
+                currency: 'usd',
+                description: '$10 for 10 credits',
+                source: req.body.id,
+                
+            })
 
         }
     )
