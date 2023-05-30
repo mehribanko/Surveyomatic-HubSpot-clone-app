b85fab5d8313969c1b62056ce7a579d0fe094cf8
MAREEKO\marym <mehrikodes@gmail.com>
2023-05-30 20:14:15 +0900

    testFromHome - configuring survey api route

--- server/routes/SurveyRoutes.js
+++ server/routes/SurveyRoutes.js
@@ -1,8 +1,17 @@
 const creditCheck = require("../middlewares/creditCheck");
 const requireLogin = require("../middlewares/requireLogin");
+const mongoose= require("mongoose");
+const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
     app.post('/api/surveys', requireLogin, creditCheck, (req,res) => {
+
+        const {title, subject, body, recipients} = req.body;
+        const survey = new Survey({
+            title,
+            subject,
+            body,})
+
     
     })          
 };
