e665471ea227ecfd9007da17ee5e7a0a146fa416
MAREEKO\marym <mehrikodes@gmail.com>
2023-05-31 19:24:50 +0900

    testFromHome - debugging

--- server/routes/SurveyRoutes.js
+++ server/routes/SurveyRoutes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
             title,
             subject,
             body,
-            recipients: recipients.split(',').map(email=> ({email})),
+            recipients: recipients.split(',').map(email=> ({email: email.trim()})),
             belongsTo: req.user.id,
             dateSent: Date.now()
         })
