97fa21e17e35158893f9ec98d33fe3605558df22
MAREEKO\marym <mehrikodes@gmail.com>
2023-06-13 20:38:35 +0900

    testFromHome - refactoring mailer service

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,7 +3,14 @@ const helper = sendGrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
-
+// whenever 'new' keyword is called, constructor is called
+    constructor({subject, recipients}, content){
+        super();
+        this.from_email = new helper.Email('mehrikodes@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 }
 
 module.exports = Mailer;
