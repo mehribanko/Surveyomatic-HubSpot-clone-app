dc69991dc845a146171c568ba39ca24b2bda0909
MAREEKO\marym <mehrikodes@gmail.com>
2023-12-17 02:45:20 +0900

    testFromHome - improving form components

--- server/client/src/components/Forms/NewSurvey.js
+++ server/client/src/components/Forms/NewSurvey.js
@@ -1,19 +1,27 @@
 import { Component } from "react";
 import Form from "./Form";
+import FormReview from "./FormReview";
 
 class NewSurvey extends Component{
+
+    state = {showReview: false};
+
+    renderContent(){
+        if(this.state.showReview){
+            return <FormReview onCancel={()=> this.setState({showReview: false})} />
+        }
+
+        return <Form onSubmit={() => this.setState({showReview: true})} />
+    }
 
     render(){
 
         return(
-
             <div>
-                <Form></Form>
+                {this.renderContent()}
             </div>
         )
     }
-
-
 }
 
 export default NewSurvey;
--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
-import {reducer as formReducer} from 'redux-form';
+import {reducer as formReducerfromRedux} from 'redux-form';
 
 export default combineReducers({
     auth: authReducer,
-    form: formReducer
+    form: formReducerfromRedux
 
 });
